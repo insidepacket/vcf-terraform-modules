cd5bb8012c4046f6724d51664bd20a6693a93636
David Zhang <david_zhangw@hotmail.com>
2024-08-13 12:06:33 +1000

    update the main.tf in the example

--- certmgmt/docs/exmaples/main.tf
+++ certmgmt/docs/exmaples/main.tf
@@ -12,11 +12,17 @@ terraform {
 }
 
 module "renew-wld-certs" {
-  source                = "./modules/certmgmt"
-  sddc_manager_username = var.sddc_manager_username
-  sddc_manager_password = var.sddc_manager_password
-  ca_secret             = var.ca_secret
-  vcf_domain_id         = "1f6d229e-1218-4033-b1cc-f0efa390d512"
-  cert_locality         = "San Francisco"
+  source                 = "./modules/certmgmt"
+  sddc_manager_username  = var.sddc_manager_username
+  sddc_manager_password  = var.sddc_manager_password
+  ca_server_url          = var.ca_server_url
+  ca_secret              = var.ca_secret
+  vcf_domain_id          = var.vcf_domain_id
+  cert_email             = var.cert_email
+  cert_key_size          = var.cert_key_size
+  cert_organization      = var.cert_organization
+  cert_organization_unit = var.cert_organization_unit
+  cert_state             = var.cert_state
+  cert_locality          = var.cert_locality
 }
 
