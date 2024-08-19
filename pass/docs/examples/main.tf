4800477debe1bdd8ad06c3416e3e932ebf7977b5
David Zhang <david_zhangw@hotmail.com>
2024-08-19 14:45:11 +1000

    update example doc to adapt to the non-legacy module

--- pass/docs/examples/main.tf
+++ pass/docs/examples/main.tf
@@ -14,6 +14,16 @@ terraform {
   }
 }
 
+provider "vcf" {
+  sddc_manager_username = var.sddc_manager_username
+  sddc_manager_password = var.sddc_manager_password
+  sddc_manager_host     = var.sddc_manager_host
+  allow_unverified_tls  = true
+}
+
+provider "vault" {
+  address = var.vault_address
+}
 module "update-vcf-esx-service-pwd" {
   source                = "./modules/pass"
   sddc_manager_username = var.sddc_manager_username
@@ -26,19 +36,4 @@ module "update-vcf-esx-service-vault" {
   source                = "./modules/support/vault"
   vault_address         = var.vault_address
   secrets = module.update-vcf-esx-service-pwd.secrets
-}
-
-
-module "update-vcf-esx-user-pwd" {
-  source                = "./modules/pass"
-  sddc_manager_username = var.sddc_manager_username
-  sddc_manager_password = var.sddc_manager_password
-  resource_type         = "ESXI"
-  account_type          = "USER"
-}
-
-module "update-vcf-esx-user-vault" {
-  source                = "./modules/support/vault"
-  vault_address         = var.vault_address
-  secrets = module.update-vcf-esx-user-pwd.secrets
 }
